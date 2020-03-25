1022f2c1e1f63c75d1fb9c4c60126bf9182edc5c
angelique-m08 <mora.angelique@gmail.com>
2020-03-25 15:52:55 +0100

    Added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const chalk = require('chalk');
+ 
+console.log(chalk.blue("Fabrice"));
+console.log(chalk.red("Anthony"));
+console.log(chalk.yellow("Victor V."));
+console.log(chalk.green("Team Wild-Post"));
+
+
